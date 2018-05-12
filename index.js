b4b476f170ae2350c6a8fddccce552a2e1afa186
Till Mueller <metjm.mail@gmail.com>
2018-05-12 08:52:19 +0200

    add missing export

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
     transactions : require("./transaction.js"),
     accounts : require("./account.js"),
     blocks : require("./block.js"),
+    api : require("./api.js"),
     utils : require("./utils.js")
 }
