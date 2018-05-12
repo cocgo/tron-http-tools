81a60c934b53c8bd571065f023efdf8feaa58b22
Till Mueller <metjm.mail@gmail.com>
2018-05-12 09:07:24 +0200

    return, not response... duh

--- api.js
+++ api.js
@@ -1,11 +1,11 @@
 const utils = require('./utils');
 
-const {Response} = require("@tronprotocol/wallet-api/src/protocol/api/api_pb");
+const {Return} = require("@tronprotocol/wallet-api/src/protocol/api/api_pb");
 
-function responseFromBase64(response){
-    return Response.deserializeBinary(utils.base64DecodeFromString(response));
+function returnFromBase64(ret){
+    return Return.deserializeBinary(utils.base64DecodeFromString(ret));
 }
 
 module.exports = {
-    responseFromBase64
+    returnFromBase64
 }
